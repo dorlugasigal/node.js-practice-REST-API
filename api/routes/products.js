377c5802fd89d2d4756a5e85e3eed917e9b2b1d9
Dor Lugasi-Gal <dorlugasigal@gmail.com>
2020-01-27 11:13:48 +0200

    :sparkles: id get

---
+++ api/routes/products.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'handling GET to /products',
+  });
+});
+
+router.post('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'handling POST to /products',
+  });
+});
+router.get('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+
+  if (id === 'special') {
+    res.status(200).json({
+      message: 'you discovered special',
+      id,
+    });
+  } else {
+    res.status(200).json({
+      message: 'you passed an ID',
+    });
+  }
+});
+
+module.exports = router;
