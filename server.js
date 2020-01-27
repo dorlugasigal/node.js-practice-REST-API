bcc047af319922dd401b81835586dd6c5ea3284f
Dor Lugasi-Gal <dorlugasigal@gmail.com>
2020-01-27 11:00:46 +0200

    :sparkles: working useless restful server

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const http = require('http');
-const express = require('express');
+const app = require('./app');
+
+const port = process.env.PORT || 3100;
+
+const server = http.createServer(app);
+
+server.listen(port);
